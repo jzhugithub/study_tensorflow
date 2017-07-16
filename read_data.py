69b8a532506404d30ba9f3af7095277f7cb744ba
jzhugithub <mfx106825372@163.com>
2017-07-16 20:41:14 +0800

    update

--- read_data.py
+++ read_data.py
@@ -61,30 +61,33 @@
 
 import tensorflow as tf
 
-filenames = ['A.csv', 'B.csv', 'C.csv']
-filename_queue = tf.train.string_input_producer(filenames, shuffle=False, num_epochs=3)
-reader = tf.TextLineReader()
-key, value = reader.read(filename_queue)
-record_defaults = [['null'], ['null']]
-example_list = [tf.decode_csv(value, record_defaults=record_defaults)
-                  for _ in range(2)]
-example_batch, label_batch = tf.train.batch_join(
-      example_list, batch_size=5, allow_smaller_final_batch=True)
+with tf.device('/cpu:0'):
+    filenames = ['A.csv', 'B.csv', 'C.csv']
+    filename_queue = tf.train.string_input_producer(filenames, shuffle=False, num_epochs=1)
+    reader = tf.TextLineReader()
+    key, value = reader.read(filename_queue)
+    record_defaults = [['null'], ['null']]
+    example_list = [tf.decode_csv(value, record_defaults=record_defaults)
+                      for _ in range(2)]
+    example_batch, label_batch = tf.train.batch_join(
+          example_list, batch_size=5, allow_smaller_final_batch=True)
 
 
 
-init_local_op = tf.initialize_local_variables()
-with tf.Session() as sess:
-    sess.run(init_local_op)
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
-    try:
-        while not coord.should_stop():
-            print example_batch.eval()
-    except tf.errors.OutOfRangeError:
-        print('Epochs Complete!')
-    finally:
+    init_local_op = tf.initialize_local_variables()
+    with tf.Session() as sess:
+        sess.run(init_local_op)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        try:
+            while not coord.should_stop():
+                eb = sess.run([example_batch])
+                print eb
+                print len(eb[0])
+        except tf.errors.OutOfRangeError:
+            print('Epochs Complete!')
+        finally:
+            coord.request_stop()
+        print('s')
         coord.request_stop()
-    print('s')
-    coord.request_stop()
-    coord.join(threads)
+        coord.join(threads)
